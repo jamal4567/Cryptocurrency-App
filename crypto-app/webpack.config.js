78fbabe506f66f5eab3a69247f05a46cda4d61ad
jamal <jamalimmiss97@gmail.com>
2025-01-22 23:16:40 +0100

    Add webpack configuration for better development experience

---
+++ crypto-app/webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = {
+  // ... other webpack configurations
+  devServer: {
+    setupMiddlewares: (middlewares, devServer) => {
+      if (!devServer) {
+        throw new Error('webpack-dev-server is not defined');
+      }
+
+      return middlewares;
+    }
+  }
+}; 
